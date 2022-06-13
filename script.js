d569c657faf864b9363c704fa169324d20de6f0f
SaiChakri-d <saichakri.d@gmail.com>
2022-06-13 16:07:00 +0530

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+// function data(){
+//     fetch("http://api.icndb.com/jokes/random").then(response=>{
+//         if (!response.ok){
+//             throw Error("error")
+//         }
+//       return response.json()
+//     }).then(data => {
+//         const content = data.value.map(d=>{
+//             return `<div class="changes"><p>ID : ${d.id} 
+// 			<br>
+// 			 Joke : ${d.joke}</p></div>`
+//         }).join("")
+//         document.querySelector("#api")
+//         .insertAdjacentHTML('afterbegin',content)
+//     }).catch(error =>{
+//         console.log(error)
+//     })
+// }
+// data();
+
 function data(){
-    fetch("https://api.icndb.com/jokes/").then(response=>{
+    fetch("http://api.icndb.com/jokes/").then(response=>{
         if (!response.ok){
             throw Error("error")
         }
       return response.json()
     }).then(data => {
         const content = data.value.map(d=>{
-            return `<div class="changes"><p>ID : ${d.id} 
-			<br>
-			 Jest : ${d.joke}</p></div>`
+            return `<div class="changes
+            "><p>ID : ${d.id} 
+                    <br> Joke : ${d.joke}</p></div>`
         }).join("")
         document.querySelector("#api")
         .insertAdjacentHTML('afterbegin',content)
@@ -16,4 +36,4 @@ function data(){
         console.log(error)
     })
 }
-data();
+data()
